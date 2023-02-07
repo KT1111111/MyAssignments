7c2403724fbe03f59eff7d0f7d6fdcfd427357c9
KT1111111 <124717733+KT1111111@users.noreply.github.com>
2023-02-07 14:39:08 -0500

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,24 @@
+public class Employee {
+    public static void main(String[] args) {
+        int salary1 = 500;
+        int salary2 = 6000;
+        int salary3 = 15000;
+
+        int l = 1000;
+        int m = 5000;
+        int h = 10000;
+        if (salary1 < l) {
+            System.out.println("Employee1 has low salary");
+
+            if (salary2 >= m)
+                System.out.println("Employee2 has med salary");
+
+            else if (salary3 >= h) ;
+            System.out.println("Employee3 has high salary");
+        }
+
+    }
+
+    }
+
+
