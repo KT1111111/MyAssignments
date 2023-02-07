a6b8a46e7b41cfccf0ae2732235b33a575b167d5
KT1111111 <124717733+KT1111111@users.noreply.github.com>
2023-02-07 13:52:45 -0500

    Add files via upload

---
+++ High.java
@@ -0,0 +1,22 @@
+public class High {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+        int c = 8;
+
+        if (a >= b && a >= c)
+
+            System.out.println(a + " is the highest number");
+
+            if (b >= a && b >= c)
+                System.out.println(b + "is the highest number");
+
+            else if (c >= a && c >= b)
+                System.out.println(c + "is the highest number");
+
+        }
+    }
+
+
+
+
